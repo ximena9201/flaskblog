dd191ccf277dc9b25cb296c2c5ae1667837b8b97
Ximena <ximenandrea.ro@gmail.com>
2021-05-30 10:29:19 -0400

    new template

--- app.py
+++ app.py
@@ -1,10 +1,8 @@
 import sqlite3
-from flask import Flask, render_template, request, url_for, flash, redirect
+from flask import Flask, render_template
 from werkzeug.exceptions import abort
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'SWdK=!rak=7gVS-='
-
 
 
 def get_db_connection():
@@ -12,21 +10,22 @@ def get_db_connection():
     conn.row_factory = sqlite3.Row
     return conn
 
-
+    
 def get_post(post_id):
     conn = get_db_connection()
     post = conn.execute('SELECT * FROM posts WHERE id = ?',
-                        (post_id,)).fetchone()
+                        (post_id,)).fetchone()  
+                        
     conn.close()
     if post is None:
         abort(404)
     return post
 
-
 @app.route('/<int:post_id>')
 def post(post_id):
     post = get_post(post_id)
     return render_template('post.html', post=post)
+
 
 
 @app.route('/')
@@ -36,10 +35,5 @@ def index():
     posts = conn.execute('SELECT * FROM posts').fetchall()
     conn.close()
     return render_template('index.html', posts=posts)
-
-
-@app.route('/create', methods=('GET', 'POST'))
-def create():
-    return render_template('create.html')
 
 
